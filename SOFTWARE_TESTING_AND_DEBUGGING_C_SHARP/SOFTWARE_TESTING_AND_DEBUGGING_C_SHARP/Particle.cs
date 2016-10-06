55fe444f710738e92553e75bd5c11644a10360e4
Pavel <catkiller95@gmail.com>
2016-10-06 20:29:31 +0300

    Добавлены все характеристики
    Добавил все характеристики из прошлого проекта, вызваемые по методам get
    set
    Необходимо уточнение понятий этих характеристик

--- SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/Particle.cs
+++ SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/Particle.cs
@@ -8,5 +8,69 @@ namespace SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP
 {
     class Particle
     {
+        private double _Square;
+        public double Square
+        {
+            get { return _Square; }
+            set { _Square = value; }
+        }
+
+        private double _Perimetr;
+        public double Perimetr
+        {
+            get { return _Perimetr; }
+            set { _Perimetr = value; }
+        }
+
+        private double _Length;
+        public double Length
+        {
+            get { return _Length; }
+            set { _Length = value; }
+        }
+
+        private double _Width;
+        public double Width
+        {
+            get { return _Width; }
+            set { _Width = value; }
+        }
+
+        private double _FormFactor;
+        public double FormFactor
+        {
+            get { return _FormFactor; }
+            set { _FormFactor = value; }
+        }
+
+        private double _Smoothness;
+        public double Smoothness
+        {
+            get { return _Smoothness; }
+            set { _Smoothness = value; }
+        }
+
+        private double _Diameter;
+        public double Diameter
+        {
+            get { return _Diameter; }
+            set { _Diameter = value; }
+        }
+
+        private double _DiameterGost;
+        public double DiameterGost
+        {
+            get { return _DiameterGost; }
+            set { _DiameterGost = value; }
+        }
+
+        private double _EllipseAngle;
+        public double EllipseAngle
+        {
+            get { return _EllipseAngle; }
+            set { _EllipseAngle = value; }
+        }
+
     }
+
 }
