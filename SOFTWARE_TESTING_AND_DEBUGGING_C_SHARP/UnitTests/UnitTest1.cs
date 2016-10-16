df044a138b07ed846c80f060a772ffa65867f781
Pavel <catkiller95@gmail.com>
2016-10-16 20:53:46 +0300

    Избавился от библиотек для выгрузки
    Добавлен тест для SquareCalculate на корректность ввода стартовой
    точки

--- SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/UnitTests/UnitTest1.cs
+++ SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/UnitTests/UnitTest1.cs
@@ -110,5 +110,35 @@ namespace SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP.UnitTests
             ProgramImage item = new ProgramImage();
             Assert.AreEqual(item.SquareCalculate(x, y, num, matrix, invalid_h, w), correctanswer);
         }
+
+        [Test]
+        public void SquareCalculate_InvalidStartPointToCalculate_Less()
+        {
+            int correctanswer = 0;
+            int x1 = -1;
+            int y1 = 0;
+            int x2 = -1;
+            int y2 = -1;
+            int x3 = 0;
+            int y3 = -1;
+            int x4 = -1;
+            int y4 = -1;
+            int h = 10;
+            int w = 10;
+            int num = 1;
+            int[,] matrix = new int[w, h];
+            for (int I = 0; I < w; I++)
+                for (int J = 0; J < h; J++)
+                    if (I == 5 && J == 5)
+                        matrix[I, J] = 1;
+                    else
+                        matrix[I, J] = 0;
+            ProgramImage item = new ProgramImage();
+            Assert.AreEqual(item.SquareCalculate(x1, y1, num, matrix, h, w), correctanswer);
+            Assert.AreEqual(item.SquareCalculate(x2, y2, num, matrix, h, w), correctanswer);
+            Assert.AreEqual(item.SquareCalculate(x3, y3, num, matrix, h, w), correctanswer);
+            Assert.AreEqual(item.SquareCalculate(x4, y4, num, matrix, h, w), correctanswer);
+        }
+
     }
 }
