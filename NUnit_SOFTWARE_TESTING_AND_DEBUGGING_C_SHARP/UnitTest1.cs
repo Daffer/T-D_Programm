d0aa2fae958bf652e04d5b843a7f48207da2bc37
Pavel <catkiller95@gmail.com>
2016-10-16 16:35:20 +0300

    Nunit подготовка
    Создан проект для тестирования основого приложения с помощью NUnit,
    Установлен Nunit.runner, Nunit, NSubtitute

---
+++ NUnit_SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace NUnit_SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP
+{
+    [TestFixture]
+    public class ProgramImage_Test
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
