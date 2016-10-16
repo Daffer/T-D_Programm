abd8f066b60d0d73d4754b62b4bccf8e082c0aaf
Pavel <catkiller95@gmail.com>
2016-10-16 20:36:27 +0300

    Добавлены 2 теста
    Добавлены тесты для проверки корректности ввода высоты

--- SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/UnitTests/UnitTest1.cs
+++ SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/UnitTests/UnitTest1.cs
@@ -68,5 +68,47 @@ namespace SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP.UnitTests
             ProgramImage item = new ProgramImage();
             Assert.AreEqual(item.SquareCalculate(x, y, num, matrix, h, invalid_w1), correctanswer);
         }
+
+        [Test]
+        public void SquareCalculate_InvalidHeightToCalculate_Less()
+        {
+            int correctanswer = -1;
+            int x = 2;
+            int y = 5;
+            int h = 10;
+            int w = 10;
+            int invalid_h = -1;
+            int num = 1;
+            int[,] matrix = new int[w, h];
+            for (int I = 0; I < w; I++)
+                for (int J = 0; J < h; J++)
+                    if (I == 5 && J == 5)
+                        matrix[I, J] = 1;
+                    else
+                        matrix[I, J] = 0;
+            ProgramImage item = new ProgramImage();
+            Assert.AreEqual(item.SquareCalculate(x, y, num, matrix, invalid_h, w), correctanswer);
+        }
+
+        [Test]
+        public void SquareCalculate_InvalidHeightToCalculate_Above()
+        {
+            int correctanswer = -2;
+            int x = 2;
+            int y = 5;
+            int h = 10;
+            int w = 10;
+            int invalid_h = 12;
+            int num = 1;
+            int[,] matrix = new int[w, h];
+            for (int I = 0; I < w; I++)
+                for (int J = 0; J < h; J++)
+                    if (I == 5 && J == 5)
+                        matrix[I, J] = 1;
+                    else
+                        matrix[I, J] = 0;
+            ProgramImage item = new ProgramImage();
+            Assert.AreEqual(item.SquareCalculate(x, y, num, matrix, invalid_h, w), correctanswer);
+        }
     }
 }
