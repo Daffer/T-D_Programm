a8d99711b3b3ffc26b9a78279fe347fbcef18855
Pavel <catkiller95@gmail.com>
2016-11-06 22:21:36 +0300

    Яркость

--- SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/FormBrightness.cs
+++ SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP/FormBrightness.cs
@@ -12,9 +12,55 @@ namespace SOFTWARE_TESTING_AND_DEBUGGING_C_SHARP
 {
     public partial class FormBrightness : Form
     {
-        public FormBrightness()
+        public FormBrightness(ProgramImage reference_to_ProgrammImage, MainForm mainmodule)
         {
             InitializeComponent();
+            ReferenceProgrammImage = reference_to_ProgrammImage;
+            ReferenceToMainForm = mainmodule;
+            return;
+        }
+
+        private ProgramImage ReferenceProgrammImage;
+        private MainForm ReferenceToMainForm;
+
+        private void BrightnessTrackBar_Scroll(object sender, EventArgs e)
+        {
+            return;
+        }        
+
+        private void ContrastTrackBar_DragDrop(object sender, DragEventArgs e)
+        {
+        }
+
+        private void ContrastTrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            int currentvalue = this.ContrastTrackBar.Value;
+            int activeimage = ReferenceToMainForm.IndexActiviteForm;
+            int error = ReferenceProgrammImage.Contrast(activeimage, currentvalue);
+            if (error == 0)
+            {
+                int number = ReferenceProgrammImage.GetCountImages();
+                Bitmap referencetomap = ReferenceProgrammImage.GetLastBitmap();
+                FormImage result = new FormImage(number, ReferenceToMainForm, referencetomap);
+                result.Show();
+                result.Focus();
+            }
+            return;
+        }
+
+        private void BrightnessTrackBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            int currentvalue = this.ContrastTrackBar.Value;
+            int activeimage = ReferenceToMainForm.IndexActiviteForm;
+            int error = ReferenceProgrammImage.Brightness(activeimage, currentvalue);
+            if (error == 0)
+            {
+                int number = ReferenceProgrammImage.GetCountImages();
+                Bitmap referencetomap = ReferenceProgrammImage.GetLastBitmap();
+                FormImage result = new FormImage(number, ReferenceToMainForm, referencetomap);
+                result.Show();
+                result.Focus();
+            }
         }
     }
 }
